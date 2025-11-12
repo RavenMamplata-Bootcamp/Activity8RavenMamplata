f286df330b30b570320e0f2261d7f98eb0ba2358
Raven Mamplata <raven.mamplata@accenture.com>
2025-11-12 13:38:27 +0000

    raven commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.training.exer8mamplata.controller.App", {
+      onInit() {
+      }
+  });
+});
